4a004828969bba33956bb539ffa52f317e56ff0a
Marwan <Marwan.kaseer1997@gmail.com>
2022-03-09 00:09:21 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace alistirma_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dizi = new string[6] {"Marwan", "Omar", "Mohammed", "Yaman", "Aliaa", "Ahmad"};
+            if (dizi.Contains("Marwan") == true)
+            Console.WriteLine("Dizide yazdigin isim bulunuyordur");
+            else
+            Console.WriteLine("Dizide yazdigin isim bulunmuyordur");
+
+
+
+        }
+    }
+}
